dc8b5feffcc03468d9a3fff0fd7c6b30f4707ff2
Nadia Sajjad <142833410+nadia-sn3@users.noreply.github.com>
2024-03-17 22:01:22 +0000

    Add files via upload
    login, signup, mainpage

---
+++ includes/footer.php
@@ -0,0 +1,32 @@
+<footer>
+
+    <div class ="footer-container">
+    
+        <div class="footer-logo">
+            <h1 class="logo-text"><a href="home.html"><span>Rose</span>Mortgage</a></h1>
+        </div>
+      
+        <div class="contact">
+            <a href="#" class="contact-address" target="_blank">
+                     Adress line 1
+                <br/>Adress line2
+                <br/>city
+                <br/>postcode
+            </a>
+            <div class="contact-details">
+                <br/><a href="mailto:#">Email: xyz</a>
+                <br/><a href="tel:#">Tel: xyz</a>
+            </div>
+        </div>
+
+    
+    <!--footer bottom-->
+        <div class="footer-bottom">
+            <div class="copyright">Rose Mortgage
+            <span></span> 2024 All Rights Reserved
+            </div>
+    
+        </div>
+    </div>
+    
+    </footer> 
---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<header>
+
+<div class="logo">
+  <h1 class="logo-text"><span>Rose</span>Mortgage</h1>
+</div>
+
+<ul class="navbar">
+  <li><a href="home.html">Home</a></li>
+  <li><a href="register.html">Sign Up</a></li>
+  <li><a href="login.html">Login</a></li>
+  <li>
+    <a href="#">
+      <i class="user"></i>
+      Name
+      <i class="dropdown"></i>
+    </a>
+
+    <ul>
+      <li><a href="#">My Profile</a></li>
+      <li><a href="#" class="logout">Logout</a></li>
+    </ul>
+
+  </li>
+</ul>
+    <!--menu icon -->
+    <div class="hamburger">
+        <span></span>
+        <span></span>
+        <span></span>
+</div>
+
+</header>
