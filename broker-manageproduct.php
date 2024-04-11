3d3ba2d0a1036db9ed397866cdc70a4ee644255e
MubKhan8889 <52501162+MubKhan8889@users.noreply.github.com>
2024-04-11 16:28:07 +0100

    Update and rename broker-manageproduct.html to broker-manageproduct.php
    products can now be displayed, but I haven't added the feature to delete them yet

---
+++ broker-manageproduct.php
@@ -0,0 +1,157 @@
+<?php 
+  session_start();
+  include("includes/connect.php");
+
+  $hasSearched = false;
+  $searchType = "None";
+  $searchText = null;
+  if (isset($_POST['search'])) {
+    $hasSearched = true;
+    $searchType = $_POST['type'];
+    $searchText = $_POST['searchproduct'];
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rose Mortgage | Home</title>
+    <link rel="stylesheet" href="style/mobile.css">
+    <link rel="stylesheet" media="only screen and (min-width:720px)" href="style/desktop.css"/>
+</head>
+
+<body>
+
+<!-- header starts -->
+<header>
+
+  <div class="logo">
+    <h1 class="logo-text"><span>Rose</span>Mortgage</h1>
+  </div>
+
+  <ul class="navbar">
+    <li><a href="broker-account.html">Account</a></li>
+    <li><a href="broker-createproduct.html">Create Product</a></li>
+    <li>
+      <a href="#">
+        <i class="user"></i>
+        <?php echo $_SESSION['user_name']; ?>
+        <i class="dropdown"></i>
+      </a>
+
+      <ul>
+        <li><a href="broker-account.html">My Profile</a></li>
+        <li><a href="#" class="logout">Logout</a></li>
+      </ul>
+
+    </li>
+  </ul>
+  <div class="hamburger">
+    <span></span>
+    <span></span>
+    <span></span>
+</div>
+
+</header>
+
+<!-- header ends -->
+
+<div class="main-container">
+
+<div class="inner-container">
+<div class="banner">
+  <div class="banner-text">
+      <h5>Welcome To</h5>
+      <h1>Rose Mortgage</h1>
+  </div>
+</div>
+
+<form class="homeform" action="broker-manageproduct.php" method="post">
+<h4>Mortgage Products</h4>
+<div class="input-group">
+  <label for="sortby">Sort by:</label>
+    <select class="control" name="type">
+      <option value="None">None</option>
+      <option value="High-Low">High-Low</option>
+      <option value="Low-High">Low-High</option>
+    </select>
+  </div>
+<div class="affordability-container">
+  <div class="input-group">
+    <label for="searchproduct">Lender: </label>
+    <input class="control" type="text" name="searchproduct" placeholder="Lender">
+    <button name="search">Search</button>
+  </div>
+</div>
+</form>
+
+<div class="productTable">
+  <?php 
+    if ($hasSearched === true) {
+      $searchQuery = "SELECT * FROM products";
+      $countQuery = "SELECT count(product_id) AS 'results' FROM products";
+
+      if ($searchText != null) { 
+        $searchQuery = $searchQuery . " WHERE lender LIKE '%{$searchText}%'";
+        $countQuery = $countQuery . " WHERE lender LIKE '%{$searchText}%'";
+      }
+
+      if ($searchType == "High-Low") { 
+        $searchQuery = $searchQuery . " ORDER BY interest_rate DESC";
+        $countQuery = $countQuery . " ORDER BY interest_rate DESC";
+      } elseif ($searchType == "Low-High") { 
+        $searchQuery = $searchQuery . " ORDER BY interest_rate ASC";
+        $countQuery = $countQuery . " ORDER BY interest_rate ASC";
+      }
+
+      $searchQuery = $searchQuery . ";";
+      $countQuery = $countQuery . ";";
+      $countResult = $mysqli->query($countQuery);
+
+      $count = 0;
+      while ($obj = $countResult->fetch_object()) {
+        $count = $obj->results;
+      }
+
+      if ($count != 0) {
+        $searchResult = $mysqli->query($searchQuery);
+
+        echo "<table>";
+        echo  "<tr>";
+        echo    "<th>Interest rate</th>";
+        echo    "<th>Mortgage term</th>";
+        echo    "<th>Lender</th>";
+        echo    "<th>Employment Status</th>";
+        echo    "<th>ITV</th>";
+        echo    "<th>Product Type</th>";
+        echo    "<th>Product Fee</th>";
+        echo    "<th>Monthly</th>";
+        echo    "<th>Mininum Age</th>";
+        echo    "<th>Maximum Age</th>";
+        echo    "<th>Mininum Credit Score</th>";
+        echo  "</tr>";
+        while ($obj = $searchResult->fetch_object()) {
+          echo  "<tr id='{$obj->product_id}' style='cursor: pointer;'>";
+          echo    "<td>{$obj->interest_rate}</td>";
+          echo    "<td>{$obj->mortgage_term}</td>";
+          echo    "<td>{$obj->lender}</td>";
+          echo    "<td>{$obj->employment_status}</td>";
+          echo    "<td>{$obj->itv}</td>";
+          echo    "<td>{$obj->product_type}</td>";
+          echo    "<td>{$obj->product_fee}</td>";
+          echo    "<td>{$obj->monthly_rate}</td>";
+          echo    "<td>{$obj->min_age}</td>";
+          echo    "<td>{$obj->max_age}</td>";
+          echo    "<td>{$obj->min_credit_score}</td>"; 
+          echo  "</tr>";
+        }
+        echo "</table>";
+      } else {
+        echo "<p>No results found</p>";
+      }
+    }
+  ?>
+</div>
+</div>
