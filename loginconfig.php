fb5474c5e67d5d759c912d3b590670d6e1a00a93
MubKhan8889 <52501162+MubKhan8889@users.noreply.github.com>
2024-04-11 14:28:21 +0100

    Update loginconfig.php
    the user can now log into broker accounts

--- loginconfig.php
+++ loginconfig.php
@@ -13,11 +13,12 @@ if ($mysqli->connect_error) {
     die("ERROR: Mysqli could not connect. " . $mysqli->connect_error);
 }
 
-function validate_login($email, $password, $mysqli) {
+function validate_login($email, $password, $mysqli, $isBroker) {
     $email = $mysqli->real_escape_string($email);
     $password = $mysqli->real_escape_string($password);
 
     $query = "SELECT * FROM users WHERE email_address = '$email' AND password = '$password'";
+    if ($isBroker === true) { $query = "SELECT * FROM brokers WHERE email_address = '$email' AND password = '$password'"; }
 
     $result = $mysqli->query($query);
 
@@ -25,7 +26,8 @@ function validate_login($email, $password, $mysqli) {
         $row = $result->fetch_assoc();
         $full_name = $row['first_name'] . " " . $row['middle_name'] . " " . $row['surname'];
         $_SESSION['user_name'] = $full_name;
-        $_SESSION['user_id'] = $row['user_id'];
+        if ($isBroker === true) { $_SESSION['user_id'] = $row['broker_id']; } else { $_SESSION['user_id'] = $row['user_id']; }
+        $_SESSION['is_broker'] = $isBroker;
         return true;
     } else {
         return false;
@@ -37,8 +39,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    if (validate_login($email, $password, $mysqli)) {
+    if (validate_login($email, $password, $mysqli, false)) {
         header("Location: UserMainPage.php");
+        exit();
+    } elseif (validate_login($email, $password, $mysqli, true)) {
+        header("Location: broker-account.html");
         exit();
     } else {
         echo "Invalid email or password.";
